a9b3bf3a9b2328e2f1775b049c5b36c4541541f6
vobizzy <volinh86@gmail.com>
2017-08-20 23:56:13 -0700

    Kickstart ARD
    This will open up all the sharing settings for ARD and VNC

---
+++ Kickstart_ARD.sh
@@ -0,0 +1,1 @@
+/System/Library/CoreServices/RemoteManagement/ARDAgent.app/Contents/Resources/kickstart -activate -configure -allowAccessFor -allUsers -privs -all -clientopts -setmenuextra -menuextra yes
